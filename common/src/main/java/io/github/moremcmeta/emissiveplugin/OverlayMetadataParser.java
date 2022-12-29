d32e34a7068259da5ac55e00fd3f7ab0ea133df8
soir20 <71418127+soir20@users.noreply.github.com>
2022-12-29 14:00:57 -0500

    Implement metadata parser

--- common/src/main/java/io/github/moremcmeta/emissiveplugin/OverlayMetadataParser.java
+++ common/src/main/java/io/github/moremcmeta/emissiveplugin/OverlayMetadataParser.java
@@ -4,10 +4,46 @@ import io.github.moremcmeta.moremcmeta.api.client.metadata.InvalidMetadataExcept
 import io.github.moremcmeta.moremcmeta.api.client.metadata.MetadataParser;
 import io.github.moremcmeta.moremcmeta.api.client.metadata.MetadataView;
 import io.github.moremcmeta.moremcmeta.api.client.metadata.ParsedMetadata;
+import net.minecraft.resources.ResourceLocation;
 
+import java.util.Optional;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Parses overlay metadata into {@link OverlayMetadata}.
+ * @author soir20
+ */
 public class OverlayMetadataParser implements MetadataParser {
     @Override
     public ParsedMetadata parse(MetadataView metadata, int imageWidth, int imageHeight) throws InvalidMetadataException {
-        return null;
+        requireNonNull(metadata, "Metadata cannot be null");
+
+        MetadataView sectionMetadata = metadata.subView(ModConstants.SECTION_NAME).orElseThrow();
+        Optional<String> rawOverlayLocation = metadata.stringValue("texture");
+        if (rawOverlayLocation.isEmpty()) {
+            throw new InvalidMetadataException("Overlays must have a texture defined");
+        }
+
+        ResourceLocation overlayLocationAsSprite = ResourceLocation.tryParse(rawOverlayLocation.get());
+        if (overlayLocationAsSprite == null) {
+            throw new InvalidMetadataException("Non [a-z0-9_.-] character in overlay texture location");
+        }
+
+        boolean isEmissive = sectionMetadata.booleanValue("emissive").orElse(false);
+
+        return new OverlayMetadata(makeTextureLocation(overlayLocationAsSprite), isEmissive);
     }
+
+    /**
+     * Converts a sprite name to a standard texture location (with textures/ prefix and .png suffix).
+     * @param location      the sprite name to convert
+     * @return the texture location corresponding to the sprite
+     */
+    private static ResourceLocation makeTextureLocation(ResourceLocation location) {
+        String originalPath = location.getPath();
+        String fullPath = "textures/" + originalPath + ".png";
+        return new ResourceLocation(location.getNamespace(), fullPath);
+    }
+
 }
