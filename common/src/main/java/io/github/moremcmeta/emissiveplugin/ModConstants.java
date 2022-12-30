8891eb62ec010b0803022fb6fd8396002c1e93e2
soir20 <71418127+soir20@users.noreply.github.com>
2022-12-29 21:00:53 -0500

    Add handling for sprite overlays

--- common/src/main/java/io/github/moremcmeta/emissiveplugin/ModConstants.java
+++ common/src/main/java/io/github/moremcmeta/emissiveplugin/ModConstants.java
@@ -1,8 +1,10 @@
 package io.github.moremcmeta.emissiveplugin;
 
+import com.google.common.collect.ImmutableSet;
 import io.github.moremcmeta.moremcmeta.api.client.metadata.MetadataParser;
 import io.github.moremcmeta.moremcmeta.api.client.texture.ComponentProvider;
 import io.github.moremcmeta.moremcmeta.api.client.texture.TextureComponent;
+import net.minecraft.resources.ResourceLocation;
 
 /**
  * Constants for both Fabric and Forge implementations of the plugin.
@@ -14,4 +16,15 @@ public class ModConstants {
     public static final String DISPLAY_NAME = "MoreMcmeta Emissive Textures";
     public static final MetadataParser PARSER = new OverlayMetadataParser();
     public static final ComponentProvider COMPONENT_PROVIDER = (metadata, frames) -> new TextureComponent<>() {};
+    public static final ImmutableSet<ResourceLocation> ATLAS_LOCATIONS = ImmutableSet.of(
+            new ResourceLocation("textures/atlas/blocks.png"),
+            new ResourceLocation("textures/atlas/signs.png"),
+            new ResourceLocation("textures/atlas/banner_patterns.png"),
+            new ResourceLocation("textures/atlas/shield_patterns.png"),
+            new ResourceLocation("textures/atlas/chest.png"),
+            new ResourceLocation("textures/atlas/beds.png"),
+            new ResourceLocation("textures/atlas/particles.png"),
+            new ResourceLocation("textures/atlas/paintings.png"),
+            new ResourceLocation("textures/atlas/mob_effects.png")
+    );
 }
--- common/src/main/java/io/github/moremcmeta/emissiveplugin/mixin/ModelPartMixin.java
+++ common/src/main/java/io/github/moremcmeta/emissiveplugin/mixin/ModelPartMixin.java
@@ -19,8 +19,8 @@ package io.github.moremcmeta.emissiveplugin.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import io.github.moremcmeta.emissiveplugin.OverlayMetadata;
 import io.github.moremcmeta.emissiveplugin.ModConstants;
+import io.github.moremcmeta.emissiveplugin.OverlayMetadata;
 import io.github.moremcmeta.emissiveplugin.WrappedBufferSource;
 import io.github.moremcmeta.moremcmeta.api.client.metadata.MetadataRegistry;
 import io.github.moremcmeta.moremcmeta.api.client.metadata.ParsedMetadata;
@@ -30,7 +30,12 @@ import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.SpriteCoordinateExpander;
+import net.minecraft.client.renderer.texture.AbstractTexture;
+import net.minecraft.client.renderer.texture.MissingTextureAtlasSprite;
+import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.resources.ResourceLocation;
+import org.apache.logging.log4j.LogManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -38,9 +43,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.Optional;
+import java.util.function.Function;
 
 @Mixin(ModelPart.class)
 public class ModelPartMixin {
+    private final Minecraft MINECRAFT = Minecraft.getInstance();
+    private ResourceLocation lastAtlas;
 
     @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;IIFFFF)V",
             at = @At(value = "HEAD"))
@@ -71,7 +79,7 @@ public class ModelPartMixin {
                 int overlayLight = overlayMetadata.isEmissive() ? LightTexture.FULL_BRIGHT : packedLight;
 
                 RenderType lastType = WrappedBufferSource.currentRenderType;
-                VertexConsumer newConsumer = bufferSource.getBuffer(RenderType.entityCutoutNoCullZOffset(overlay));
+                VertexConsumer newConsumer = makeBuffer(bufferSource, overlay, RenderType::entityCutoutNoCullZOffset);
                 thisPart.render(poseStack, newConsumer, overlayLight, packedOverlay, red, blue, green, alpha);
 
                 // Restore original render type
@@ -81,5 +89,84 @@ public class ModelPartMixin {
         }
 
         WrappedBufferSource.depth--;
+    }
+
+    private VertexConsumer makeBuffer(MultiBufferSource bufferSource, ResourceLocation overlayLocation,
+                                      Function<ResourceLocation, RenderType> renderTypeFunction) {
+        ResourceLocation spriteName = makeSpriteName(overlayLocation);
+        Optional<VertexConsumer> spriteBuffer;
+
+        // Check the cached atlas before checking all atlases
+        if (lastAtlas != null) {
+            spriteBuffer = makeBufferIfSprite(
+                    lastAtlas,
+                    overlayLocation,
+                    spriteName,
+                    bufferSource,
+                    renderTypeFunction
+            );
+
+            if (spriteBuffer.isPresent()) {
+                return spriteBuffer.get();
+            }
+        }
+
+        // Check all atlases if the texture is no longer in that atlas
+        for (ResourceLocation atlasLocation : ModConstants.ATLAS_LOCATIONS) {
+            spriteBuffer = makeBufferIfSprite(
+                    atlasLocation,
+                    overlayLocation,
+                    spriteName,
+                    bufferSource,
+                    renderTypeFunction
+            );
+
+            if (spriteBuffer.isPresent()) {
+                lastAtlas = atlasLocation;
+                return spriteBuffer.get();
+            }
+        }
+
+        // Texture isn't a sprite
+        return bufferSource.getBuffer(renderTypeFunction.apply(overlayLocation));
+
+    }
+
+    private Optional<VertexConsumer> makeBufferIfSprite(ResourceLocation atlasLocation,
+                                                        ResourceLocation overlayLocation,
+                                                        ResourceLocation spriteName,
+                                                        MultiBufferSource bufferSource,
+                                                        Function<ResourceLocation, RenderType> renderTypeFunction) {
+        AbstractTexture abstractTexture = MINECRAFT.getTextureManager().getTexture(atlasLocation);
+        if (!(abstractTexture instanceof TextureAtlas atlas)) {
+            LogManager.getLogger().warn(
+                    "Atlas {} is not a subclass of TextureAtlas; sprites from this atlas will not be used as overlays",
+                    atlasLocation
+            );
+            return Optional.empty();
+        }
+
+        TextureAtlasSprite sprite = atlas.getSprite(spriteName);
+        if (sprite.getName().equals(MissingTextureAtlasSprite.getLocation())) {
+            sprite = atlas.getSprite(overlayLocation);
+        }
+
+        if (!sprite.getName().equals(MissingTextureAtlasSprite.getLocation())) {
+            RenderType renderType = renderTypeFunction.apply(atlasLocation);
+            return Optional.of(sprite.wrap(bufferSource.getBuffer(renderType)));
+        }
+
+        return Optional.empty();
+    }
+
+    private ResourceLocation makeSpriteName(ResourceLocation textureLocation) {
+        int prefixLength = "textures/".length();
+        int postfixLength = ".png".length();
+        int pathLength = textureLocation.getPath().length();
+
+        return new ResourceLocation(
+                textureLocation.getNamespace(),
+                textureLocation.getPath().substring(prefixLength, pathLength - postfixLength)
+        );
     }
 }
