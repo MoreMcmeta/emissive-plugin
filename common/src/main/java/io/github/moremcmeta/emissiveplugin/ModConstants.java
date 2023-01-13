776c1b74780c6be45c8bb69fb5e552f20943ec5f
soir20 <71418127+soir20@users.noreply.github.com>
2023-01-13 00:23:06 -0500

    Remove unused list of atlas locations

--- common/src/main/java/io/github/moremcmeta/emissiveplugin/ModConstants.java
+++ common/src/main/java/io/github/moremcmeta/emissiveplugin/ModConstants.java
@@ -1,6 +1,5 @@
 package io.github.moremcmeta.emissiveplugin;
 
-import com.google.common.collect.ImmutableSet;
 import io.github.moremcmeta.emissiveplugin.metadata.OverlayMetadata;
 import io.github.moremcmeta.emissiveplugin.metadata.OverlayMetadataParser;
 import io.github.moremcmeta.moremcmeta.api.client.metadata.MetadataParser;
@@ -21,17 +20,6 @@ public class ModConstants {
     public static final String DISPLAY_NAME = "MoreMcmeta Emissive Textures";
     public static final MetadataParser PARSER = new OverlayMetadataParser();
     public static final ComponentProvider COMPONENT_PROVIDER = (metadata, frames) -> new TextureComponent<>() {};
-    public static final ImmutableSet<ResourceLocation> ATLAS_LOCATIONS = ImmutableSet.of(
-            new ResourceLocation("textures/atlas/blocks.png"),
-            new ResourceLocation("textures/atlas/signs.png"),
-            new ResourceLocation("textures/atlas/banner_patterns.png"),
-            new ResourceLocation("textures/atlas/shield_patterns.png"),
-            new ResourceLocation("textures/atlas/chest.png"),
-            new ResourceLocation("textures/atlas/beds.png"),
-            new ResourceLocation("textures/atlas/particles.png"),
-            new ResourceLocation("textures/atlas/paintings.png"),
-            new ResourceLocation("textures/atlas/mob_effects.png")
-    );
     public static final Consumer<Consumer<ResourceLocation>> SPRITE_REGISTRAR_CONSUMER = (registrar) ->
             MetadataRegistry.INSTANCE.metadataByPlugin(ModConstants.DISPLAY_NAME).values().forEach(
                     (metadata) -> registrar.accept(((OverlayMetadata) metadata).overlayLocation())
