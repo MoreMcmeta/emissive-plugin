0d7d77b09bc7c540ab935743bf257f61fe160215
soir20 <71418127+soir20@users.noreply.github.com>
2023-06-21 01:02:55 -0400

    Update Architectury Loom, Gradle

--- common/src/main/java/io/github/moremcmeta/emissiveplugin/render/WrappedBufferSource.java
+++ common/src/main/java/io/github/moremcmeta/emissiveplugin/render/WrappedBufferSource.java
@@ -22,14 +22,12 @@ import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 
-import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.function.Consumer;
 
 /**
  * Wraps render buffer sources so that an action is run when a buffer is retrieved.
  * @author soir20
  */
-@ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public final class WrappedBufferSource implements MultiBufferSource {
     private final MultiBufferSource DELEGATE;
