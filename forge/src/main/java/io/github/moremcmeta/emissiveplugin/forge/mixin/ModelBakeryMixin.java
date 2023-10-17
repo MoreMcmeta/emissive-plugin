bd8f27058f3611c4c13ed8990299f0bbb6633c00
soir20 <71418127+soir20@users.noreply.github.com>
2023-10-17 00:04:03 -0400

    Fix overlays for redstone on Forge

--- forge/src/main/java/io/github/moremcmeta/emissiveplugin/forge/mixin/ModelBakeryMixin.java
+++ forge/src/main/java/io/github/moremcmeta/emissiveplugin/forge/mixin/ModelBakeryMixin.java
@@ -25,7 +25,6 @@ import net.minecraft.client.resources.model.BuiltInModel;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.client.resources.model.ModelState;
-import net.minecraft.client.resources.model.MultiPartBakedModel;
 import net.minecraft.client.resources.model.WeightedBakedModel;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
@@ -84,7 +83,7 @@ public final class ModelBakeryMixin {
 
         // Built-in models are empty, and wrapping them causes shulker boxes, etc. to be invisible in the inventory
         if (usesOverlay && !(original instanceof OverlayBakedItemModel) && !(original instanceof BuiltInModel)
-                && !(original instanceof MultiPartBakedModel) && !(original instanceof WeightedBakedModel)) {
+                && !(original instanceof WeightedBakedModel)) {
             callbackInfo.setReturnValue(
                     new OverlayBakedItemModel(callbackInfo.getReturnValue())
             );
