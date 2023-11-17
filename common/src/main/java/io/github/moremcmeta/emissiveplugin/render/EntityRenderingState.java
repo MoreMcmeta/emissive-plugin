ee45a48b00f0dceff584342612c777a307076e67
soir20 <71418127+soir20@users.noreply.github.com>
2023-11-17 00:45:40 -0500

    Fix armor afterimage, fog covering overlays with Oculus

--- common/src/main/java/io/github/moremcmeta/emissiveplugin/render/EntityRenderingState.java
+++ common/src/main/java/io/github/moremcmeta/emissiveplugin/render/EntityRenderingState.java
@@ -18,6 +18,7 @@
 package io.github.moremcmeta.emissiveplugin.render;
 
 import io.github.moremcmeta.emissiveplugin.mixin.ModelPartMixin;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 
 /**
@@ -29,6 +30,7 @@ public final class EntityRenderingState {
     /* Entity and block entity rendering should be single-threaded, but use thread locals to
        avoid difficult bugs in case something changes. */
     public static final ThreadLocal<RenderType> currentRenderType = new ThreadLocal<>();
+    public static final ThreadLocal<MultiBufferSource> currentBufferSource = new ThreadLocal<>();
     public static final ThreadLocal<Integer> partRenderDepth = ThreadLocal.withInitial(() -> -1);
     public static final ThreadLocal<Boolean> isBlockEntity = ThreadLocal.withInitial(() -> false);
 
